2b0af0753ec86ad115a6f579542334f40212c315
Peyton Meredith <peymeredith@gmail.com>
2023-10-30 16:11:51 -0500

    game tracker mock challenge

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -3,32 +3,110 @@ class Game:
         self.title = title
 
     def results(self):
-        pass
+        return [results for results in Result.all if results.game == self]
 
     def players(self):
-        pass
+        return list({result.player for result in self.results() if isinstance(result.player, Player)})
 
     def average_score(self, player):
-        pass
+        if len(self.results()) == 0:
+            return 0
+        player_scores = [result.score for result in self.results() if result.player == player]
+        return sum(player_scores) / len(player_scores)
+
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, title):
+        if not isinstance(title, str):
+            raise TypeError("Title must be a string")
+        if len(title) < 1:
+            raise ValueError("Title must be 1 or more characters")
+        if hasattr(self, "_title"):
+            raise AttributeError("Title can not be changed")
+        self._title = title
 
 class Player:
+    all = []
+
     def __init__(self, username):
         self.username = username
+        Player.all.append(self)
 
     def results(self):
-        pass
+        return [results for results in Result.all if results.player == self]
 
     def games_played(self):
-        pass
+        return list({result.game for result in self.results() if isinstance(result.game, Game)})
 
     def played_game(self, game):
-        pass
+        if game in self.games_played():
+            return True
+        return False
 
     def num_times_played(self, game):
-        pass
+        play_count = 0
+        for result in self.results():
+            if isinstance(result.game, Game) and result.game == game:
+                play_count += 1
+        return play_count
+
+    @property
+    def username(self):
+        return self._username
+    
+    @username.setter
+    def username(self, username):
+        if not isinstance(username, str):
+            raise TypeError("username must be a string")
+        if len(username) < 2 or len(username) > 16:
+            raise ValueError("username must be between 2 to 16 characters")
+        self._username = username
+
+    @classmethod
+    def highest_scored(cls, game):
+        players = game.players()
+        if not players:
+            return None
+        best_player = max(players, key=lambda player: game.average_score(player))
+        return best_player
 
 class Result:
+    all = []
     def __init__(self, player, game, score):
         self.player = player
         self.game = game
         self.score = score
+        Result.all.append(self)
+
+    @property
+    def player(self):
+        return self._player
+    
+    @player.setter
+    def player(self, player):
+        self._player = player
+
+    @property
+    def game(self):
+        return self._game
+    
+    @game.setter
+    def game(self, game):
+        self._game = game
+
+    @property
+    def score(self):
+        return self._score
+    
+    @score.setter
+    def score(self, score):
+        if not isinstance(score, int):
+            raise TypeError("score must be an integer")
+        if score < 1 or score > 5000:
+            raise ValueError("score must be between 2 to 16 characters")
+        if hasattr(self, "_score"):
+            raise AttributeError("Score can not be changed")
+        self._score = score
--- lib/debug.py
+++ lib/debug.py
@@ -8,4 +8,26 @@ from classes.many_to_many import Result
 if __name__ == '__main__':
     print("HELLO! :) let's debug :vibing_potato:")
 
+    g1 = Game("Monopoly")
+    g2 = Game("Settlers of Catan")
+    g3 = Game("Risk")
+    g4 = Game("Werewolf")
+
+    p1 = Player("nymeria")
+    p2 = Player("tony.two.hands")
+    p3 = Player("otto.hands")
+
+    r1 = Result(p1, g1, 350)
+    r2 = Result(p1, g2, 2000)
+    r3 = Result(p1, g4, 1500)
+    r4 = Result(p2, g3, 5000)
+    r5 = Result(p2, g1, 700)
+    r6 = Result(p2, g4, 700)
+    r7 = Result(p3, g1, 1900)
+    r8 = Result(p3, g3, 4500)
+    r9 = Result(p3, g2, 1450)
+    r10 = Result(p3, g4, 3500)
+    r11 = Result(p1, g1, 3800)
+
     ipdb.set_trace()
+
